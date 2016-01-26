d12d5b20788839aede9cd3c1e766ee420e3d0ed9
jojydevasia <jojydevasia1@gmail.com>
2016-01-26 18:07:58 +0530

    Initial Checking of Actitime

---
+++ ActiTime/src/com/actitime/AcTmOpenTaskTab.java
@@ -0,0 +1,42 @@
+package com.actitime;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class AcTmOpenTaskTab {
+	
+	WebDriver driver;
+	
+	String openTskPgTitle=null;
+	By addNewTskBTN= By.xpath("//input[@value='Add New Tasks']");
+	By custList= By.xpath("//select[@name='customerProject.shownCustomer']");
+	By projList= By.xpath("//select[@name='customerProject.shownProject']");
+	By showTskBtn=By.name("searchTasks");
+	
+	public AcTmOpenTaskTab(WebDriver driver){
+		this.driver=driver;
+		openTskPgTitle=driver.getTitle();
+	}
+	
+	public void clickNewTskBtn(){
+		driver.findElement(addNewTskBTN).click();
+	}
+	
+	public void selCustList(String customer){
+		Select sel1=new Select(driver.findElement(custList));
+		sel1.selectByVisibleText(customer);
+	}
+	
+	public void selProjList(String project){
+		Select sel2=new Select(driver.findElement(projList));
+		sel2.selectByVisibleText(project);
+	}
+	
+	public void clickShowTaskBtn(){
+		driver.findElement(showTskBtn).click();
+	}
+
+}
